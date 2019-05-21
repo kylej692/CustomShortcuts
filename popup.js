81bb9bd232848f79e08368de7aec9155a7cab32c
Kyle Jang <kyle.jang@mail.utoronto.ca>
2019-05-21 17:15:25 -0400

    Refactor popup.js to use jquery

--- popup.js
+++ popup.js
@@ -1,121 +1,122 @@
 ﻿var shortcuts = [];
-document.addEventListener('DOMContentLoaded', function() { 
+$(function() {
   getShortcutsAndRestoreInDom();
-  document.getElementById('saveShortcut1').addEventListener('click', function() {
-    shortcuts[0] = document.getElementById("shortcut1").value;
+  $('#saveShortcut1').click(function() {
+    shortcuts[0] = $('#shortcut1').val();
     chrome.runtime.sendMessage({"message": "shortcut1", "value": shortcuts[0]});
     saveShortcuts();
   })
-  document.getElementById('reset1').addEventListener('click', function() {
+  $('#reset1').click(function() {
     shortcuts[0] = "";
     chrome.runtime.sendMessage({"message": "shortcut1", "value": shortcuts[0]});
-    document.getElementById('shortcut1').value = "";
+    $('#shortcut1').val("");
     saveShortcuts();
   })
-  document.getElementById('saveShortcut2').addEventListener('click', function() {
-    shortcuts[1] = document.getElementById("shortcut2").value;
+  $('#saveShortcut2').click(function() {
+    shortcuts[1] = $('#shortcut2').val();
     chrome.runtime.sendMessage({"message": "shortcut2", "value": shortcuts[1]});
     saveShortcuts();
   })
-  document.getElementById('reset2').addEventListener('click', function() {
+  $('#reset2').click(function() {
     shortcuts[1] = "";
     chrome.runtime.sendMessage({"message": "shortcut2", "value": shortcuts[1]});
-    document.getElementById('shortcut2').value = "";
+    $('#shortcut2').val("");
     saveShortcuts();
   })
-  document.getElementById('saveShortcut3').addEventListener('click', function() {
-    shortcuts[2] = document.getElementById("shortcut3").value;
+  $('#saveShortcut3').click(function() {
+    shortcuts[2] = $('#shortcut3').val();
     chrome.runtime.sendMessage({"message": "shortcut3", "value": shortcuts[2]});
     saveShortcuts();
   })
-  document.getElementById('reset3').addEventListener('click', function() {
+  $('#reset3').click(function() {
     shortcuts[2] = "";
     chrome.runtime.sendMessage({"message": "shortcut3", "value": shortcuts[2]});
-    document.getElementById('shortcut3').value = "";
+    $('#shortcut3').val("");
     saveShortcuts();
   })
-  document.getElementById('saveShortcut4').addEventListener('click', function() {
-    shortcuts[3] = document.getElementById("shortcut4").value;
+  $('#saveShortcut4').click(function() {
+    shortcuts[3] = $('#shortcut4').val();
     chrome.runtime.sendMessage({"message": "shortcut4", "value": shortcuts[3]});
     saveShortcuts();
   })
-  document.getElementById('reset4').addEventListener('click', function() {
+  $('#reset4').click(function() {
     shortcuts[3] = "";
     chrome.runtime.sendMessage({"message": "shortcut4", "value": shortcuts[3]});
-    document.getElementById('shortcut4').value = "";
+    $('#shortcut4').val("");
     saveShortcuts();
   })
-  document.getElementById('saveShortcut5').addEventListener('click', function() {
-    shortcuts[4] = document.getElementById("shortcut5").value;
+  $('#saveShortcut5').click(function() {
+    shortcuts[4] = $('#shortcut5').val();
     chrome.runtime.sendMessage({"message": "shortcut5", "value": shortcuts[4]});
     saveShortcuts();
   })
-  document.getElementById('reset5').addEventListener('click', function() {
+  $('#reset5').click(function() {
     shortcuts[4] = "";
     chrome.runtime.sendMessage({"message": "shortcut5", "value": shortcuts[4]});
-    document.getElementById('shortcut5').value = "";
+    $('#shortcut5').val("");
     saveShortcuts();
   })
-  document.getElementById('saveShortcut6').addEventListener('click', function() {
-    shortcuts[5] = document.getElementById("shortcut6").value;
+  $('#saveShortcut6').click(function() {
+    shortcuts[5] = $('#shortcut6').val();
     chrome.runtime.sendMessage({"message": "shortcut6", "value": shortcuts[5]});
     saveShortcuts();
   })
-  document.getElementById('reset6').addEventListener('click', function() {
+  $('#reset6').click(function() {
     shortcuts[5] = "";
     chrome.runtime.sendMessage({"message": "shortcut6", "value": shortcuts[5]});
-    document.getElementById('shortcut6').value = "";
+    $('#shortcut6').val("");
     saveShortcuts();
   })
-  document.getElementById('saveShortcut7').addEventListener('click', function() {
-    shortcuts[6] = document.getElementById("shortcut7").value;
+  $('#saveShortcut7').click(function() {
+    shortcuts[6] = $('#shortcut7').val();
     chrome.runtime.sendMessage({"message": "shortcut7", "value": shortcuts[6]});
     saveShortcuts();
   })
-  document.getElementById('reset7').addEventListener('click', function() {
+  $('#reset7').click(function() {
     shortcuts[6] = "";
     chrome.runtime.sendMessage({"message": "shortcut7", "value": shortcuts[6]});
-    document.getElementById('shortcut7').value = "";
+    $('#shortcut7').val("");
     saveShortcuts();
   })
-  document.getElementById('saveShortcut8').addEventListener('click', function() {
-    shortcuts[7] = document.getElementById("shortcut8").value;
+  $('#saveShortcut8').click(function() {
+    shortcuts[7] = $('#shortcut8').val();
     chrome.runtime.sendMessage({"message": "shortcut8", "value": shortcuts[7]});
     saveShortcuts();
   })
-  document.getElementById('reset8').addEventListener('click', function() {
+  $('#reset8').click(function() {
     shortcuts[7] = "";
     chrome.runtime.sendMessage({"message": "shortcut8", "value": shortcuts[7]});
-    document.getElementById('shortcut8').value = "";
+    $('#shortcut8').val("");
     saveShortcuts();
   })
 });
+
 function getShortcutsAndRestoreInDom(){
     chrome.storage.local.get({shortcuts:[]},function(data){
         shortcuts = data.shortcuts;
         if(shortcuts[0] != undefined) {
-          document.getElementById("shortcut1").value = shortcuts[0];
+          $('#shortcut1').val(shortcuts[0]);
           chrome.runtime.sendMessage({"message": "shortcut1", "value": shortcuts[0]});
         } if(shortcuts[1] != undefined) {
-          document.getElementById("shortcut2").value = shortcuts[1];
+          $('#shortcut2').val(shortcuts[1]);
           chrome.runtime.sendMessage({"message": "shortcut2", "value": shortcuts[1]});
         } if(shortcuts[2] != undefined) {
-          document.getElementById("shortcut3").value = shortcuts[2];
+          $('#shortcut3').val(shortcuts[2]);
           chrome.runtime.sendMessage({"message": "shortcut3", "value": shortcuts[2]});
         } if(shortcuts[3] != undefined) {
-          document.getElementById("shortcut4").value = shortcuts[3];
+          $('#shortcut4').val(shortcuts[3]);
           chrome.runtime.sendMessage({"message": "shortcut4", "value": shortcuts[3]});
         } if(shortcuts[4] != undefined) {
-          document.getElementById("shortcut5").value = shortcuts[4];
+          $('#shortcut5').val(shortcuts[4]);
           chrome.runtime.sendMessage({"message": "shortcut5", "value": shortcuts[4]});
         } if(shortcuts[5] != undefined) {
-          document.getElementById("shortcut6").value = shortcuts[5];
+          $('#shortcut6').val(shortcuts[5]);
           chrome.runtime.sendMessage({"message": "shortcut6", "value": shortcuts[5]});
         } if(shortcuts[6] != undefined) {
-          document.getElementById("shortcut7").value = shortcuts[6];
+          $('#shortcut7').val(shortcuts[6]);
           chrome.runtime.sendMessage({"message": "shortcut7", "value": shortcuts[6]});
         } if(shortcuts[7] != undefined) {
-          document.getElementById("shortcut8").value = shortcuts[7];
+          $('#shortcut8').val(shortcuts[7]);
           chrome.runtime.sendMessage({"message": "shortcut8", "value": shortcuts[7]});
         }
     });
